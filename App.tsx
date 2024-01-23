98e3971d6337543cc36633b2e66dcbb36c486fce
adrianojec <adrianojeric.28@gmail.com>
2024-01-23 23:53:55 +0800

    feat: added bottom sheet
    - added bottom sheet package
    - added dependencies for bottom sheet
      - react-native-reanimated
      - react-native-gesture-handler
      - must install using `npx expo install package-name` to get compatible version for the SDK
    - wrapped App.tsx with:
      - GestureHandlerRootView
      - BottomSheetProvider

--- App.tsx
+++ App.tsx
@@ -3,13 +3,19 @@ import { BottomTabNavigator } from "./src/navigation/BottomTabNavigator";
 import { AuthStackNavigator } from "./src/navigation/AuthStackNavigator";
 import { Provider } from "react-redux";
 import { store } from "./src/store/store";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
     <NavigationContainer>
       <Provider store={store}>
         {/* <AuthStackNavigator /> */}
-        <BottomTabNavigator />
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <BottomSheetModalProvider>
+            <BottomTabNavigator />
+          </BottomSheetModalProvider>
+        </GestureHandlerRootView>
       </Provider>
     </NavigationContainer>
   );
